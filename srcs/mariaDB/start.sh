bf534d79abc6f54354348adfd7422b530032f2ab
Emil Ebert <emilebert@icloud.com>
2025-04-15 14:13:15 +0200

    Add creation of administrator user in start.sh

--- srcs/mariaDB/start.sh
+++ srcs/mariaDB/start.sh
@@ -16,8 +16,15 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
     # Configure database
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE:-wordpress};"
+
+    # Create regular user
     mysql -e "CREATE USER '${MYSQL_USER:-wordpress}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD:-wordpress}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE:-wordpress}.* TO '${MYSQL_USER:-wordpress}'@'%';"
+
+    # Create administrator user (not containing any form of 'admin')
+    mysql -e "CREATE USER '${WP_ADMIN_USER:-superuser}'@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD:-superpass}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE:-wordpress}.* TO '${WP_ADMIN_USER:-superuser}'@'%' WITH GRANT OPTION;"
+
     mysql -e "FLUSH PRIVILEGES;"
 
     # Stop temporary server
